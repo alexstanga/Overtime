face9685ebf523577969289292b5b3b80ad80f33
Alex <alex.stanga@me.com>
2017-02-24 22:18:18 -0700

    created partial for full coverage on grower notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    redirect_to root_path, notice: "You are not authorized to perform this action."
+    redirect_to root_path, alert: "You are not authorized to perform this action."
   end
 end
